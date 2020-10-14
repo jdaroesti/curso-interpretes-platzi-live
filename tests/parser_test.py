c259f9daa301728623d7070cf4627bc4c3f19a2c
David Aroesti <josedavid.aroesti@gmail.com>
2020-10-14 12:13:39 -0500

    Parsing let statements - challenge

--- tests/parser_test.py
+++ tests/parser_test.py
@@ -1,3 +1,7 @@
+from typing import (
+    cast,
+    List,
+)
 from unittest import TestCase
 
 from lpp.ast import (
@@ -36,4 +40,25 @@ class ParserTest(TestCase):
         for statement in program.statements:
             self.assertEqual(statement.token_literal(), 'variable')
             self.assertIsInstance(statement, LetStatement)
+
+    def test_names_in_let_statements(self) -> None:
+        source: str = '''
+            variable x = 5;
+            variable y = 10;
+            variable foo = 20;
+        '''
+        lexer: Lexer = Lexer(source)
+        parser: Parser = Parser(lexer)
+
+        program: Program = parser.parse_program()
+
+        names: List[str] = []
+        for statement in program.statements:
+            statement = cast(LetStatement, statement)
+            assert statement.name is not None
+            names.append(statement.name.value)
+
+        expected_names: List[str] = ['x', 'y', 'foo']
+
+        self.assertEquals(names, expected_names)
 
