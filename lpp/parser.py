ee7a8db429321bb0cb29703b5b64aef2077eaedb
David Aroesti <josedavid.aroesti@gmail.com>
2020-10-14 12:09:20 -0500

    Parsing let statments part 2

--- lpp/parser.py
+++ lpp/parser.py
@@ -1,6 +1,11 @@
 from typing import Optional
 
-from lpp.ast import Program
+from lpp.ast import (
+    Identifier,
+    LetStatement,
+    Program,
+    Statement,
+)
 from lpp.lexer import Lexer
 from lpp.token import (
     Token,
@@ -15,6 +20,9 @@ class Parser:
         self._current_token: Optional[Token] = None
         self._peek_token: Optional[Token] = None
 
+        self._advance_tokens()
+        self._advance_tokens()
+
     def parse_program(self) -> Program:
         program: Program = Program(statements=[])
 
@@ -24,9 +32,42 @@ class Parser:
             if statement is not None:
                 program.statements.append(statement)
 
+            self._advance_tokens()
+
         return program
 
-    def _parse_statement(self) -> Optional[Statment]:
+    def _advance_tokens(self) -> None:
+        self._current_token = self._peek_token
+        self._peek_token = self._lexer.next_token()
+
+    def _expected_token(self, token_type: TokenType) -> bool:
+        assert self._peek_token is not None
+        if self._peek_token.token_type == token_type:
+            self._advance_tokens()
+
+            return True
+
+        return False
+
+    def _parse_let_statement(self) -> Optional[LetStatement]:
+        assert self._current_token is not None
+        let_statement = LetStatement(token=self._current_token)
+
+        if not self._expected_token(TokenType.IDENT):
+            return None
+
+        let_statement.name = Identifier(token=self._current_token, value=self._current_token.literal)
+
+        if not self._expected_token(TokenType.ASSIGN):
+            return None
+
+        # TODO terminar cuando sepamos parsear expresiones
+        while self._current_token.token_type != TokenType.SEMICOLON:
+            self._advance_tokens()
+
+        return let_statement
+
+    def _parse_statement(self) -> Optional[Statement]:
         assert self._current_token is not None
         if self._current_token.token_type == TokenType.LET:
             return self._parse_let_statement()
