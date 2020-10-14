b6f0e050579cb7f14316187a8350ecc4354fb577
David Aroesti <josedavid.aroesti@gmail.com>
2020-10-14 09:53:54 -0500

    First REPL

---
+++ main.py
@@ -0,0 +1,12 @@
+from lpp.repl import start_repl
+
+
+def main() -> None:
+    print('Bienvenido al Lenguaje de Programación Platzi.')
+    print('Escribe un oración para comenzar.')
+
+    start_repl()
+
+
+if __name__ == '__main__':
+    main()
