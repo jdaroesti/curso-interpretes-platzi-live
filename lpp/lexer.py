20d20931eb9fdfaac7cbac4869bf7d78e827d475
David Aroesti <josedavid.aroesti@gmail.com>
2020-10-14 09:29:11 -0500

    Two character operator

--- lpp/lexer.py
+++ lpp/lexer.py
@@ -21,7 +21,10 @@ class Lexer:
         self._skip_whitespace()
 
         if match(r'^=$', self._character):
-            token = Token(TokenType.ASSIGN, self._character)
+            if self._peek_character() == '=':
+                token = self._make_two_character_token(TokenType.EQ)
+            else:
+                token = Token(TokenType.ASSIGN, self._character)
         elif match(r'^\+$', self._character):
             token = Token(TokenType.PLUS, self._character)
         elif match(r'^$', self._character):
@@ -49,7 +52,10 @@ class Lexer:
         elif match(r'^>$', self._character):
             token = Token(TokenType.GT, self._character)
         elif match(r'^!$', self._character):
-            token = Token(TokenType.NEGATION, self._character)
+            if self._peek_character() == '=':
+                token = self._make_two_character_token(TokenType.NOT_EQ)
+            else:
+                token = Token(TokenType.NEGATION, self._character)
         elif self._is_letter(self._character):
             literal = self._read_identifier()
             token_type = lookup_token_type(literal)
@@ -71,6 +77,13 @@ class Lexer:
 
     def _is_number(self, character: str) -> bool:
         return bool(match(r'^\d$', character))
+
+    def _make_two_character_token(self, token_type: TokenType) -> Token:
+        prefix = self._character
+        self._read_character()
+        suffix = self._character
+
+        return Token(token_type, f'{prefix}{suffix}')
 
     def _read_character(self) -> None:
         if self._read_position >= len(self._source):
@@ -96,6 +109,12 @@ class Lexer:
             self._read_character()
 
         return self._source[initial_position:self._position]
+
+    def _peek_character(self) -> str:
+        if self._read_position >= len(self._source):
+            return ''
+
+        return self._source[self._read_position]
 
     def _skip_whitespace(self) -> None:
         while match(r'^\s$', self._character):
