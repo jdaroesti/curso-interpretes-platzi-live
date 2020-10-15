3219e8c8c289012d5fc75632804033c77e0a32ba
David Aroesti <josedavid.aroesti@gmail.com>
2020-10-15 12:01:26 -0500

    Parse integer literals

--- lpp/ast.py
+++ lpp/ast.py
@@ -106,3 +106,17 @@ class ExpressionStatement(Statement):
 
     def __str__(self) -> str:
         return str(self.expression)
+
+
+class Integer(Expression):
+
+    def __init__(self,
+                 token: Token,
+                 value: Optional[int] = None) -> None:
+        super().__init__(token)
+        self.value = value
+
+    def __str__(self) -> str:
+        return str(self.value)
+
+
